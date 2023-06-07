9b616d1a2351e0c675c49f50179c4b4d56487338
Lucas Rezac <Lucas.Rezac@syngenta.com>
2023-06-07 15:33:14 -0500

    Docstrings

--- pyadapt/schemas/__init__.py
+++ pyadapt/schemas/__init__.py
@@ -1,3 +1,8 @@
+"""pyadapt.schemas - JSON Schema validation utilities.
+
+Author: Lucas Rezac <lucas.rezac@syngenta.com>
+"""
+
 import json
 from functools import lru_cache, wraps
 from pathlib import Path
@@ -10,6 +15,18 @@ from jsonschema import ValidationError as ValidationError
 from jsonschema.validators import RefResolver
 from typing_extensions import ParamSpec
 
+__all__ = [
+    "JSONPrimitive",
+    "JSONObject",
+    "JSONArray",
+    "JSONValue",
+    "check_uuid",
+    "check_uuid_uuid",
+    "validate",
+    "validate_result",
+    "get_schema",
+]
+
 JSONPrimitive = Union[None, bool, int, float, str]
 JSONObject = Dict[str, "JSONValue"]
 JSONArray = List["JSONValue"]
@@ -18,7 +35,9 @@ JSONValue = Union[JSONPrimitive, JSONObject, JSONArray]
 _R = TypeVar("_R", bound=JSONValue)
 _P = ParamSpec("_P")
 
-resolver = RefResolver(base_uri=f"{Path(__file__).parent.as_uri()}/", referrer=cast(Any, True))
+resolver = RefResolver(
+    base_uri=f"{Path(__file__).parent.as_uri()}/", referrer=cast(Any, True)
+)
 format_checker = FormatChecker()
 
 
@@ -41,7 +60,12 @@ def check_uuid_uuid(value: object) -> bool:
 
 
 def validate(value: JSONValue, schema: str):
-    jsonschema.validate(instance=value, schema=get_schema(schema), resolver=resolver, format_checker=format_checker)
+    jsonschema.validate(
+        instance=value,
+        schema=get_schema(schema),
+        resolver=resolver,
+        format_checker=format_checker,
+    )
 
 
 def validate_result(schema: Union[str, JSONObject]):
@@ -52,14 +76,19 @@ def validate_result(schema: Union[str, JSONObject]):
     >>> @validate_result("Asset")
     def get_asset(...) -> ...: ...
 
-    will require the return value of `get_asset` to validate `syngenta_databus_shared/json_schemas/Asset.schema.json`."""
+    will require the return value of `get_asset` to validate `pyadapt/schemas/Asset.schema.json`."""
     schema = get_schema(schema)
 
     def decorator(func: Callable[_P, _R], /) -> Callable[_P, _R]:
         @wraps(func)
         def decorated(*args: _P.args, **kwargs: _P.kwargs) -> _R:
             result = func(*args, **kwargs)
-            jsonschema.validate(instance=result, schema=schema, resolver=resolver, format_checker=format_checker)
+            jsonschema.validate(
+                instance=result,
+                schema=schema,
+                resolver=resolver,
+                format_checker=format_checker,
+            )
             return result
 
         return decorated
